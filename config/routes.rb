dd1e826a0d071ac20756739eb382bde39083e8a0
Bastian Caceres <bcaceres5582@e-camp.cl>
2021-09-15 20:40:59 -0300

    Tercer Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/three'
   get 'pages/one'
   get 'pages/two'
+  
   root 'pages#one'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
